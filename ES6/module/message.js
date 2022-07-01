95125825616fbeb6c4937039ba961063a08021cc
kabitaremotework <104657984+kabitaremotework@users.noreply.github.com>
2022-07-02 04:23:37 +0545

    module
    module

---
+++ ES6/module/message.js
@@ -0,0 +1,7 @@
+
+export let message = "I need to send this message to script.js"
+
+
+export let sum = function() {
+    return 30;
+}
---
+++ ES6/module/script.js
@@ -0,0 +1,5 @@
+import { message, sum } from "./message.js";
+
+console.log(message)
+
+console.log(sum())
