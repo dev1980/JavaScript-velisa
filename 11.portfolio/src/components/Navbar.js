49b64cb96a72eeff9efb674e2a84eb87f63f6411
Brham Dev Mahato <brham1980@gmail.com>
2022-09-28 15:29:21 +0545

    update resume link

--- 11.portfolio/src/components/Navbar.js
+++ 11.portfolio/src/components/Navbar.js
@@ -22,6 +22,7 @@ setExpandNavbar(false)
         <Link to="/">Home</Link>
         <Link to="/work">Work</Link>
         <Link to="/contact">Contact</Link>
+        <a href='https://docs.google.com/document/d/15d4b3Gvq1eV8KHUpmkp-h53l49V-drhkV7FW8KHQCSI/edit?usp=sharing' target="_blank">Resume</a>
 
         </div>
     </div>
